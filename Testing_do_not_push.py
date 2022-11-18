e3fe452e967df182d8b925471965206abf022afd
Ivan_1 <mail@mai.com>
2022-11-18 11:35:23 +0300

    Добавил функцию перевода в цвет

---
+++ Testing_do_not_push.py
@@ -0,0 +1,10 @@
+import tkinter
+from tkinter.filedialog import *
+from solar_vis import *
+from solar_model import *
+from solar_input import *
+from solar_objects import *
+
+#read_space_objects_data_from_file("solar_system.txt")
+
+#print(from_rgb(color_f("red")))
